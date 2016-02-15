21dc6e501f35491fbe678bdfe974d165659dc63b
Bootta <goldcashclub@gmail.com>
2016-02-15 23:48:13 +0100

    First succesful comit page2 finished

---
+++ TestGtkGui/AnimatedButton.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Gtk;
+using System.Threading;
+
+namespace TestGtkGui
+{
+	public class AnimatedButton: EventBox{
+		Gdk.Pixbuf imgNormal=null;
+		Gdk.Pixbuf imgHover=null;
+		Gdk.Pixbuf imgPressed=null;
+		Image img=null;
+		string lastState="";
+		Thread onReleaseThread=null;
+		ThreadStart orbStart=null;
+		bool pressed=false;
+		static Gdk.Color defaultBgColor = new Gdk.Color (255,255,255);
+
+		public static void setButtonsDefaultBgColor(Gdk.Color defbgColor){
+			defaultBgColor = defbgColor;
+		}
+
+		public AnimatedButton(Gdk.Pixbuf normal, Gdk.Pixbuf hover, Gdk.Pixbuf pressed){
+			this.AboveChild=true;
+			this.imgNormal=normal;
+			this.imgHover=hover;
+			this.imgPressed=pressed;
+			img=new Image(this.imgNormal);
+			img.Show();
+			this.Add(img);
+
+			this.EnterNotifyEvent+=new EnterNotifyEventHandler(onEnterWidget);
+			this.LeaveNotifyEvent+=new LeaveNotifyEventHandler(onLeaveWidget);
+			this.ButtonPressEvent+=new ButtonPressEventHandler(onPressWidget);
+			this.ButtonReleaseEvent+=new ButtonReleaseEventHandler(onReleasedWidget);
+			this.Show();
+			orbStart=new ThreadStart(empty_run);
+			onReleaseThread=new Thread(orbStart);
+			this.ModifyBg (StateType.Normal, defaultBgColor);
+		}
+
+		protected void onEnterWidget(object o, EnterNotifyEventArgs args){
+			Console.WriteLine (args.ToString());
+			if (!pressed) {
+				img.Pixbuf = this.imgHover;
+				lastState = "enter";
+				Console.WriteLine (args.ToString()+" Done");
+			} else {
+				Console.WriteLine (args.ToString()+" Not done. Button pressed.");
+			}
+		}
+
+		protected void onLeaveWidget(object o, LeaveNotifyEventArgs args){
+			Console.WriteLine (args.ToString());
+			if(!pressed){
+				img.Pixbuf = this.imgNormal;
+				lastState = "leave";
+				Console.WriteLine (args.ToString()+" Done");
+			} else {
+				Console.WriteLine (args.ToString()+" Not done. Button pressed.");
+			}
+		}
+
+		protected void onPressWidget(object o, ButtonPressEventArgs args){
+			Console.WriteLine (args.ToString());
+			img.Pixbuf = this.imgPressed;
+			pressed = true;
+			lastState = "press";
+		}
+
+		protected void onReleasedWidget(object o, ButtonReleaseEventArgs args){
+			Console.WriteLine (args);
+			img.Pixbuf = this.imgHover;
+			onReleaseThread=new Thread(orbStart);
+			onReleaseThread.Start ();
+			pressed = false;
+			lastState = "release";
+		}
+
+		static void empty_run(){
+			Console.WriteLine ("Empty thread ready method");
+		}
+
+		public void setOnReleaseMethod(ThreadStart threadStartUsingMethod){
+			orbStart = threadStartUsingMethod;
+		}
+
+	}
+}
+
---
+++ TestGtkGui/MainWindow.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Gtk;
+using TestGtkGui;
+
+public partial class MainWindow: Gtk.Window
+{
+	String subtitlesFontSize="14000";
+	String tempMaskValue="";
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		notebook2.Page = 1;
+		//notebook2.GetNthPage (0).Hide (); //hide first page
+		//notebook2.GetNthPage (2).Hide (); //hide third page
+		//label15.Text = "Hi";
+		Gdk.Color col = new Gdk.Color(138, 196, 74);
+		//Gdk.Color.Parse("red", ref col);
+		this.ModifyBg (StateType.Normal, col);
+
+		lblWgDetails.Markup="<span size='"+subtitlesFontSize+"' color='green' weight='bold'>WG Details</span>";
+		lblEinzugsdatum.Markup="<span size='"+subtitlesFontSize+"' color='green' weight='bold'>Einzugsdatum</span>";
+		//btnPreferences.CanFocus = false;
+
+		statusbar2.Push (1, "Ready");
+		 
+		//statusbar2.PackStart(new Image("run.png"), false, false, 0); 
+		//btnForward2.Image = Image.LoadFromResource ("TestGtkGui.wg_detektiv_logo.png");
+		AnimatedButton.setButtonsDefaultBgColor(col);
+
+		AnimatedButton btnSchritt1Animated = new AnimatedButton (new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"));
+		AnimatedButton btnSchritt2Animated = new AnimatedButton (new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"));
+		AnimatedButton btnSchritt3Animated = new AnimatedButton (new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"),new Gdk.Pixbuf("images\\schritt2.png"));
+		AnimatedButton btnPreferencesAnimated = new AnimatedButton (new Gdk.Pixbuf("images\\preferences_normal.png"),new Gdk.Pixbuf("images\\preferences_hover.png"),new Gdk.Pixbuf("images\\preferences_pressed.png"));
+
+		//btnSchritt2Animated.wid
+		hbox25.PackEnd (btnSchritt1Animated);
+		hbox25.PackEnd (btnSchritt2Animated);
+		hbox26.PackStart (btnSchritt3Animated);
+		hbox26.PackEnd (btnPreferencesAnimated);
+
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+	protected void OnTxtMinGrosseFocusGrabbed (object sender, EventArgs e)
+	{
+		hideTxtMask (sender);
+	}
+
+	protected void OnTxtMinGrosseFocusOutEvent (object o, FocusOutEventArgs args)
+	{
+		showTxtMask (o);
+	}
+
+	protected void showTxtMask(object o){
+		Entry txtField = (Entry)o;
+		if (txtField.Text == "") {
+			txtField.Text=tempMaskValue;
+			txtField.Xalign = 1;
+			tempMaskValue = "";
+		}
+
+	}
+
+	protected void hideTxtMask(object o){
+		Entry txtField = (Entry)o;
+		if (txtField.Xalign == 1) {
+			tempMaskValue = txtField.Text;
+			txtField.Text = "";
+			txtField.Xalign = 0;
+		}
+	}
+
+	protected void OnTxtMaxMieteFocusGrabbed (object sender, EventArgs e)
+	{
+		hideTxtMask (sender);
+	}
+
+	protected void OnTxtMaxMieteFocusOutEvent (object o, FocusOutEventArgs args)
+	{
+		showTxtMask (o);
+	}
+
+	protected void OnToggleCalendarFromActivated (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+	protected void OnToggleCalendarFromToggled (object sender, EventArgs e)
+	{
+		ToggleButton tglBtn = (ToggleButton)sender;
+		if (tglBtn.Active) {
+			calFrom.Show ();
+		} else {
+			calFrom.Hide();
+		}
+	}
+
+	protected void OnToggleCalendarToToggled (object sender, EventArgs e)
+	{
+		ToggleButton tglBtn = (ToggleButton)sender;
+		if (tglBtn.Active) {
+			calTo.Show ();
+		} else {
+			calTo.Hide();
+		}
+	}
+
+	protected void OnCalFromDaySelected (object sender, EventArgs e)
+	{
+		Calendar cal = (Calendar)sender;
+		String date = String.Format("{0:d/M/yyyy}",cal.GetDate());
+		txtFruhestens.Text = date;
+	}
+
+	protected void OnCalFromDaySelectedDoubleClick (object sender, EventArgs e)
+	{
+		Calendar cal = (Calendar)sender;
+		String date = String.Format("{0:d/M/yyyy}",cal.GetDate());
+		txtFruhestens.Text = date;
+		cal.Hide ();
+		toggleCalendarFrom.Active=false;
+
+	}
+
+	protected void OnCalToDaySelected (object sender, EventArgs e)
+	{
+		Calendar cal = (Calendar)sender;
+		String date = String.Format("{0:d/M/yyyy}",cal.GetDate());
+		txtSpatestens.Text = date;
+	}
+
+	protected void OnCalToDaySelectedDoubleClick (object sender, EventArgs e)
+	{
+		Calendar cal = (Calendar)sender;
+		String date = String.Format("{0:d/M/yyyy}",cal.GetDate());
+		txtSpatestens.Text = date;
+		cal.Hide ();
+		toggleCalendarTo.Active=false;
+	}
+
+	protected void OnNotebook2SelectPage (object o, SelectPageArgs args)
+	{
+		//notebook2.GetNthPage (1);
+	}
+
+	protected void OnNotebook2ChangeCurrentPage (object o, ChangeCurrentPageArgs args)
+	{
+		//notebook2.Page = 1;
+	}
+
+	protected void OnBtnForward2Clicked (object sender, EventArgs e)
+	{
+		notebook2.Page = 0;
+	}
+
+	protected void OnNotebook2SwitchPage (object o, SwitchPageArgs args)
+	{
+		notebook2.Page = 1;
+	}
+}
